7bc23a7636a95edeb72ebc56239a8b800f492721
wided kh <khediriwided@gmail.com>
2023-12-22 12:01:37 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function showAlert() {
+    var Text = "Your cart is empty!";
+    alert (Text);
+  }
+
+  function OnHover(element)
+  {element.src='///C:/Users/ASUS/Documents/exam/images/assets/succulentst-2.jpg'
+  }
+  
+
+  function OffHover(element){
+   
+    element.src='///C:/Users/ASUS/Documents/exam/images/assets/succulents-1.jpg'
+}
+
+
+
+    var  hide= document.querySelector(".cookies");
+    function accept() {
+       hide.remove();
+    }
